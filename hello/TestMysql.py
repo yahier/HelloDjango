4055cc3c92f5afef0f9d5980e5435c40d38fcf6b
xuziwang <xuziwang@stbl.cc>
2016-12-07 16:27:38 +0800

    新增对mysql的测试。结果成功

--- hello/TestMysql.py
+++ hello/TestMysql.py
@@ -1,4 +1,6 @@
+# coding:utf-8
 import MySQLdb
+
 
 def insert():
     conn = MySQLdb.connect(host="localhost", user="root", passwd="2008", db="yahier");
@@ -8,4 +10,22 @@ def insert():
     conn.commit();
 
 
-insert()
+def getDBConnect():
+    return MySQLdb.connect(host="localhost", user="root", passwd="2008", db="yahier");
+
+
+def getDBCursor():
+    conn = getDBConnect()
+    return conn.cursor();
+
+
+def updateIndex(index):
+    sql = "update user set name = 'bingo' where id = 2"
+    conn = getDBConnect();
+    cursor = conn.cursor();
+    line = cursor.execute(sql);
+    print("影响的行数" + str(line))
+    conn.commit();
+
+
+updateIndex(1);
