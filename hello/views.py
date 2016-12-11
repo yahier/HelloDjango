49efe3bee9ecebdc4fccdcc18cbc43a8d37ec71e
yahier <xuziwang@gmail.com>
2016-12-11 20:53:57 +0800

    增删一些文件和增加笔记文件

--- hello/views.py
+++ hello/views.py
@@ -10,16 +10,19 @@ from django.template import Context
 from django.shortcuts import render_to_response
 import datetime
 
-# Create your views here.
+
 def hello(request):
     return HttpResponse("<h1>Hello,World HERE IS CHINA</h1")
+
 
 def ya(request):
     return HttpResponse("hello,yahier")
 
+
 #怎么输出json格式呢
 def people(request):
     return HttpResponse("hello,people啊")
+
 
 def gallery(request):
     t = get_template('gallery.html')
@@ -29,6 +32,7 @@ def gallery(request):
 
 def html2(request):
     return render_to_response('gallery.html')
+
 
 def current_datetime(request):
     now = datetime.datetime.now()
