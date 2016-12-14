da35eccfdfedb8b03475c0508859d8ee0aad20cf
yahier <xuziwang@gmail.com>
2016-12-14 21:02:02 +0800

    配置模型 写入mysql数据库成。

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,21 @@
 #coding:utf-8
 from django.db import models
+#测试发现，文件名不是任意的
 
-#生成表是用新命令 migrations
+
 class Mysite(models.Model):
     title = models.CharField(max_length=100)
-    url = models.URLField()
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    num = models.IntegerField(max_length=10)
+
+
+#可以成功写入英文。但数据提示需要配置这个相关 DEFAULT_INDEX_TABLESPACE
+#两个字段都是中文的话，有时成功，有时候不成功
+def test():
+   book = Book(name='english', author='who')
+   book.save()
+
+test()
